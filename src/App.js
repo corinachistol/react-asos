3c6d7b1cfecde85ad74114555625da9f72b65f4d
corinachistol <chistolcorina19@gmail.com>
2023-12-26 15:35:36 +0200

    split style container signin into header&main button signin&join, error message

--- src/App.js
+++ src/App.js
@@ -9,42 +9,50 @@ function App() {
   const [errors,setErrors] = useState({})
   const [submitting, setSubmitting] = useState(false)
 
+  const [buttonSelected, setButtonSelected] = useState("")
+
+  function handleClick(e){
+    setButtonSelected(e.target.innerText)
+  }
+
   return (
     <>
-    <Logo/>
+      <Logo/>
 
-    <div className="container-sign">
+      <div className="container-sign">
 
-      {Object.keys(errors).length === 0 && submitting ? 
-        (<span className="success">Successfully signed up ✔. Please check your <a href="#">email</a> for confirmation!</span>) 
-        : 
-        (<div className='grid-container'>
-        <Button 
-          clas='signin-options'
-          text="JOIN"/>
+        {Object.keys(errors).length === 0 && submitting ? 
+          (<span className="success">Successfully signed up ✔. Please check your <a href="#">email</a> for confirmation!</span>) 
+          : (
+            <>
+              <div className='container-signin__header'>
+                <Button 
+                  clas='signin-options'
+                  text="JOIN"
+                  clickHandler={handleClick}/>
 
-        <Button 
-          clas='signin-options'
-          text="SIGN IN"/>
+                <Button 
+                  clas='signin-options'
+                  text="SIGN IN"
+                  clickHandler={handleClick}/>
 
+              </div>
 
-        <Join 
-          errors={errors}
-          onSetErrors={setErrors}
-          submitting={submitting}
-          onSetSubmitting={setSubmitting}/>
-        
-        {/* <FormSignIn/> */}
-       
-         </div>
+              <div className="container-signin__main">
+                {buttonSelected === "JOIN" && <Join 
+                    errors={errors}
+                    onSetErrors={setErrors}
+                    submitting={submitting}
+                    onSetSubmitting={setSubmitting} />
+                }
 
-        )}
-      
-
-     
-      
-
-    </div>
+                {buttonSelected === "SIGN IN" && <FormSignIn/>}
+               
+                
+              </div>  
+            </>
+          )}
+      </div>
     </>
   );
 }
